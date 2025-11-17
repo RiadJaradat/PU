f673b656f24dc553023325ea6e4244365835988a
RiadJaradat <riadaljaradat@gmail.com>
2025-11-17 18:35:21 +0300

    initial commit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-pip install -r requirements.txt > null
+pip install -r requirements.txt > /dev/null 2>&1
 python3 Main.py
 echo "Program exited with exit code:" $?
 deactivate
